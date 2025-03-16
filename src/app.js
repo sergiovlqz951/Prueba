39ec8cde9f5d6d49bb33212ce29ac52d4abd31e3
sergiovlqz951 <sergiovlqz951@gmail.com>
2025-03-16 15:30:46 -0400

    Base con rutas

--- src/app.js
+++ src/app.js
@@ -24,24 +24,6 @@ app.use('/productos', productosRouter);
 app.use('/compras', comprasRouter);
 app.use('/detalles-compra.js', detallesCompraRouter)
 
-
-
- // Endpoint para insertar un nuevo cliente
-    app.post('/clientes', (req, res) => {
-    //const { nombre, direccion, telefono, email } = req.body; // Datos enviados en la solicitud
-    //const query = 'INSERT INTO clientes (nombre, direccion, telefono, email) VALUES (?, ?, ?, ?)';
-  
-    // db.query(query, [nombre, direccion, telefono, email], (err, results) => {
-    //   if (err) {
-    //     console.error('Error al insertar el cliente:', err);
-    //     res.status(500).send('Error al crear el cliente');
-    //   } else {
-    //     res.status(201).send('Cliente creado exitosamente');
-    //   }
-    // });
-    res.json(req.body)
-  });
-  
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
